228f3d52fb141b676231c5f1e6c9bf9bd6d530b3
Mats Rauhala <mats.rauhala@gmail.com>
2010-09-09 20:16:55 +0300

    Fix colors to work with zsh prompt

--- Color.hs
+++ Color.hs
@@ -22,13 +22,13 @@ data Color = Blue String |
     deriving (Show)
 
 instance CColor Color where
-    render (Black s) = "\o033[0;30m" ++ s ++ "\o33[0m"
-    render (Red s) = "\o033[0;31m" ++ s ++ "\o33[0m"
-    render (Green s) = "\o033[0;32m" ++ s ++ "\o33[0m"
-    render (Brown s) = "\o033[0;33m" ++ s ++ "\o33[0m"
-    render (Yellow s) = "\o033[1;33m" ++ s ++ "\o33[0m"
-    render (Blue s) = "\o033[0;34m" ++ s ++ "\o33[0m"
-    render (Purple s) = "\o033[0;35m" ++ s ++ "\o33[0m"
+    render (Black s)  = "%{\o033[0;30m%}" ++ s ++ "%{\o33[0;0m%}"
+    render (Red s)    = "%{\o033[0;31m%}" ++ s ++ "%{\o33[0;0m%}"
+    render (Green s)  = "%{\o033[0;32m%}" ++ s ++ "%{\o33[0;0m%}"
+    render (Brown s)  = "%{\o033[0;33m%}" ++ s ++ "%{\o33[0;0m%}"
+    render (Yellow s) = "%{\o033[1;33m%}" ++ s ++ "%{\o33[0;0m%}"
+    render (Blue s)   = "%{\o033[0;34m%}" ++ s ++ "%{\o33[0;0m%}"
+    render (Purple s) = "%{\o033[0;35m%}" ++ s ++ "%{\o33[0;0m%}"
 
 class CColor a where
     render :: a -> String
